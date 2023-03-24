629bd45347d1602d9b344dfda7233a2c2f112d34
Nick Tassinari <ntass7800@gmail.com>
2023-03-24 14:55:01 -0600

    add count method

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -13,4 +13,28 @@ RSpec.describe LinkedList do
     expect(list.head).to eq(nil)
   end
 
+  it 'can append new data' do 
+    list = LinkedList.new 
+    list.append("doop")
+    expect(list.head.data).to eq("doop")
+  end
+
+  it 'has no next node for the moment' do 
+    list = LinkedList.new 
+    list.append("doop")
+    expect(list.head.next_node).to eq(nil)
+  end
+
+  it 'has a list count' do 
+    list = LinkedList.new 
+    list.append("doop")
+    expect(list.count).to eq(1)
+  end
+
+  it 'has to string method' do 
+    list = LinkedList.new 
+    list.append("doop")
+    expect(list.to_string).to eq("doop")
+  end
+
 end
