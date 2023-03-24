30aa74c8af7363a019f9f0092678ca353c14fef1
Nick Tassinari <ntass7800@gmail.com>
2023-03-24 14:55:49 -0600

    add count method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -12,8 +12,31 @@ class LinkedList
     end
   end 
 
-  
-  
+  def count 
+    # create a count with if statement   
+    #if the head is nil the count should be 0
+    #else  the count should be 1 and the node is equal to @head
+
+    #until node.nextnode is nil
+    #add one to counter and node = node.next_node 
+    #return the count 
+    if @head.nil? 
+      0
+    else 
+      counter = 1 
+      node = @head 
+
+    until node.next_node.nil? 
+      counter += 1
+      node = node.next_node 
+    end   
+      counter 
+    end
+  end 
+
+  def to_string 
+    
+  end
 
 
 end
