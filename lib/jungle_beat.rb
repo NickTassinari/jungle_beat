8b841d273596ee3f32ce3cf0182cea98abae233e
Nick Tassinari <ntass7800@gmail.com>
2023-03-28 14:30:43 -0600

    finish jungle_massive

---
+++ lib/jungle_beat.rb
@@ -0,0 +1,27 @@
+class JungleBeat 
+  attr_reader :list 
+
+  def initialize 
+    @list = LinkedList.new 
+  end
+
+  def append(nodes_data)
+    nodes = nodes_data.split(' ')
+
+    nodes.each do |node|
+      @list.append(node)
+    end
+
+    nodes_data
+  end
+
+
+  def count 
+    @list.count 
+  end
+
+  def play 
+    beats = @list.to_string 
+    `say -r 75 #{beats}`
+  end
+end
